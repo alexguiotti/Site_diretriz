a3aa382769c77d793d316e7210bb20ca98bb0555
alexguiotti <110726306+alexguiotti@users.noreply.github.com>
2025-12-11 11:50:07 -0300

    domini

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'https://localhost:3001';
+const API_URL = import.meta.env.VITE_API_URL || 'http://diretrizautomotiva.com.br';
 
 const getAuthHeaders = () => {
     const token = localStorage.getItem('authToken');
